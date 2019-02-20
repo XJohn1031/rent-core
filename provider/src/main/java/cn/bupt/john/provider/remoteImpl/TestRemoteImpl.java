b672b61aba1404a4f38ad1e99ce54da0f3f80ef7
XJohn <xj124116211@qq.com>
2019-02-20 20:14:03 +0800

    add

---
+++ provider/src/main/java/cn/bupt/john/provider/remoteImpl/TestRemoteImpl.java
@@ -0,0 +1,20 @@
+package cn.bupt.john.provider.remoteImpl;
+
+import cn.bupt.john.remote.TestRemote;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author xjohn
+ * @since 20 February 2019
+ */
+@Service
+public class TestRemoteImpl implements TestRemote {
+
+	@Override
+	public String testRemoteMethod(String request) {
+		return "success";
+	}
+
+}
